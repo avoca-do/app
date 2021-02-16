e87e843fc1ac8c6d40683f4e5c010064a93f3356
vaux <zero.griffin@gmail.com>
2021-02-16 18:39:23 +0100

    Update icons

--- macOS/Window.swift
+++ macOS/Window.swift
@@ -16,34 +16,6 @@ final class Window: NSWindow {
         accesory.view = NSView()
         accesory.layoutAttribute = .top
         addTitlebarAccessoryViewController(accesory)
-        
-        let a = NSView()
-        a.translatesAutoresizingMaskIntoConstraints = false
-        a.wantsLayer = true
-        a.layer!.backgroundColor = .init(gray: 0.2, alpha: 1)
-        
-        let b = NSView()
-        b.wantsLayer = true
-        b.translatesAutoresizingMaskIntoConstraints = false
-        b.layer!.backgroundColor = .init(gray: 0.8, alpha: 1)
-        
-        let split = NSSplitView()
-        split.isVertical = true
-        split.translatesAutoresizingMaskIntoConstraints = false
-//        split.addArrangedSubview(a)
-//        split.addArrangedSubview(b)
-        split.insertArrangedSubview(a, at: 0)
-        split.insertArrangedSubview(b, at: 1)
-        split.adjustSubviews()
-        split.coll
-        contentView!.addSubview(split)
-        
-        print(split.isSubviewCollapsed(a))
-        print(split.isSubviewCollapsed(b))
-        
-        split.topAnchor.constraint(equalTo: contentView!.safeAreaLayoutGuide.topAnchor).isActive = true
-        split.bottomAnchor.constraint(equalTo: contentView!.safeAreaLayoutGuide.bottomAnchor).isActive = true
-        split.leftAnchor.constraint(equalTo: contentView!.safeAreaLayoutGuide.leftAnchor).isActive = true
-        split.rightAnchor.constraint(equalTo: contentView!.safeAreaLayoutGuide.rightAnchor).isActive = true
+
     }
 }
