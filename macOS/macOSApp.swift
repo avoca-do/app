13a72c564e84c1167f11fa02693868306dbcacb3
vaux <zero.griffin@gmail.com>
2021-02-16 14:41:20 +0100

    Bump version

---
+++ macOS/macOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  macOSApp.swift
+//  macOS
+//
+//  Created by vaux on 16.02.21.
+//
+
+import SwiftUI
+
+@main
+struct macOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
