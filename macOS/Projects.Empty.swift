7823909c27f73a979f4bf4225c1c4062e7ee2532
vaux <zero.griffin@gmail.com>
2021-02-18 16:13:48 +0100

    Update empty

--- macOS/Projects.Empty.swift
+++ macOS/Projects.Empty.swift
@@ -5,6 +5,14 @@ extension Projects {
         required init?(coder: NSCoder) { nil }
         init() {
             super.init(frame: .zero)
+            let text = Text()
+            text.stringValue = NSLocalizedString("Click +\nto start a project", comment: "")
+            text.font = .preferredFont(forTextStyle: .title3)
+            text.textColor = .secondaryLabelColor
+            addSubview(text)
+            
+            text.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor).isActive = true
+            text.leftAnchor.constraint(equalTo: safeAreaLayoutGuide.leftAnchor, constant: 50).isActive = true
         }
     }
 }
