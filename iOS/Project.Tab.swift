edee5d1704c1938c028d6ff2a63e969bd50a52e8
vaux <zero.griffin@gmail.com>
2021-03-01 12:45:00 +0100

    Update tabs

--- iOS/Project.Tab.swift
+++ iOS/Project.Tab.swift
@@ -1,13 +1,27 @@
 import SwiftUI
+import Kanban
 
 extension Project {
     struct Tab: View {
+        @Binding var session: Session
+        let path: Kanban.Path
+        
         var body: some View {
             ZStack {
                 RoundedRectangle(cornerRadius: Metrics.corners)
                     .fill(Color(.secondarySystemBackground))
-                ScrollView {
-                    
+                VStack {
+                    HStack {
+                        Text(verbatim: session.archive[title: path])
+                            .font(Font.body.bold())
+                        Spacer()
+                        Text(NSNumber(value: session.archive.count(path)), formatter: session.decimal)
+                            .foregroundColor(.secondary)
+                    }
+                    .padding([.top, .horizontal])
+                    ScrollView {
+                        
+                    }
                 }
             }
             .padding(.horizontal)
--- iOS/Project.swift
+++ iOS/Project.swift
@@ -2,16 +2,31 @@ import SwiftUI
 
 struct Project: View {
     @Binding var session: Session
+    @State private var tab = 0
+    @State private var count = 1
     
     var body: some View {
         Title(session: $session)
-        TabView {
-            Tab()
-            Tab()
-            Tab()
+        TabView(selection: $tab) {
+            ForEach(0 ..< count, id: \.self) {
+                Tab(session: $session, path: .column(session.path, $0))
+                    .tag($0)
+            }
         }
         .tabViewStyle(PageTabViewStyle())
         .edgesIgnoringSafeArea(.horizontal)
         Options(session: $session)
+            .onAppear(perform: refresh)
+            .onChange(of: session.path) { _ in
+                refresh()
+            }
+    }
+    
+    private func refresh() {
+        tab = 0
+        switch session.path {
+        case .board: count = session.archive.count(session.path)
+        default: count = 1
+        }
     }
 }
