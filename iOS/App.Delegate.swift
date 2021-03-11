d611e8db0760a19629552e31340f372ffa2629d7
vaux <zero.griffin@gmail.com>
2021-03-11 17:23:49 +0100

    Update register

--- iOS/App.Delegate.swift
+++ iOS/App.Delegate.swift
@@ -4,15 +4,15 @@ import Kanban
 
 extension App {
     final class Delegate: NSObject, UIApplicationDelegate {
-        func application(_: UIApplication, willFinishLaunchingWithOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-            UIApplication.shared.registerForRemoteNotifications()
+        func application(_ application: UIApplication, willFinishLaunchingWithOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+            application.registerForRemoteNotifications()
             Memory.shared.load()
             
             DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
                 if let created = Defaults.created {
                     if !Defaults.rated && Calendar.current.dateComponents([.day], from: created, to: .init()).day! > 4 {
                         Defaults.rated = true
-                        SKStoreReviewController.requestReview(in: UIApplication.shared.windows.first!.windowScene!)
+                        SKStoreReviewController.requestReview(in: application.windows.first!.windowScene!)
                     }
                 } else {
                     Defaults.created = .init()
