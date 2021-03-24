cde86b6d07abab2318244a02ccb494f0f1d3bd3a
vaux <zero.griffin@gmail.com>
2021-03-24 14:26:37 +0100

    Update ring

--- iOS/Progress.Ring.swift
+++ iOS/Progress.Ring.swift
@@ -6,9 +6,12 @@ extension Progress {
         
         func path(in rect: CGRect) -> Path {
             .init {
-                let center = CGPoint(x: rect.width / 2, y: rect.height / 2)
-                let radius = (min(rect.width, rect.height) / 2) - Metrics.progress.stroke
-                $0.addArc(center: center, radius: radius, startAngle: .init(degrees: -90), endAngle: .init(degrees: (360 * amount) - 90), clockwise: false)
+                $0.addArc(
+                    center: .init(x: rect.width / 2, y: rect.height / 2),
+                    radius: (min(rect.width, rect.height) / 2) - Metrics.progress.stroke,
+                    startAngle: .init(degrees: -90),
+                    endAngle: .init(degrees: (360 * amount) - 90),
+                    clockwise: false)
             }
         }
         
