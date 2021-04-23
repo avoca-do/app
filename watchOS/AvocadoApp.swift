77affcff2e94331723c66215999748da56d0d698
vaux <zero.griffin@gmail.com>
2021-04-23 11:42:16 +0200

    Add watch target

---
+++ watchOS/AvocadoApp.swift
@@ -0,0 +1,19 @@
+//
+//  AvocadoApp.swift
+//  watchOS Extension
+//
+//  Created by vaux on 23.04.21.
+//
+
+import SwiftUI
+
+@main
+struct AvocadoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
