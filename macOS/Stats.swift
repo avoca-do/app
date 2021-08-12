22277e3409f1cf0bbdba320b46a10d0dc2aa2dd2
vaux <zero.griffin@gmail.com>
2021-08-12 12:48:41 +0200

    Update stats

--- macOS/Stats.swift
+++ macOS/Stats.swift
@@ -1,38 +1,93 @@
 import AppKit
+import Combine
 
 final class Stats: NSPopover {
     required init?(coder: NSCoder) { nil }
     init(board: Int) {
         super.init()
         behavior = .semitransient
-        contentSize = .init(width: 400, height: 400)
+        contentSize = .init(width: 300, height: 390)
         contentViewController = .init()
         contentViewController!.view = .init(frame: .init(origin: .zero, size: contentSize))
+        view.wantsLayer = true
         
         let progress = cloud.archive.value[board].progress
+        
+        let ring = Shape()
+        ring.frame = .init(x: 30, y: 40, width: 240, height: 240)
+        ring.strokeColor = NSColor.labelColor.withAlphaComponent(0.05).cgColor
+        ring.fillColor = .clear
+        ring.lineWidth = 10
+        ring.path = {
+            $0.addArc(center: .init(x: 120, y: 120), radius: 115, startAngle: 0, endAngle: 2 * .pi, clockwise: false)
+            return $0
+        } (CGMutablePath())
+        view.layer!.addSublayer(ring)
+        
+        let current = Shape()
+        current.frame = .init(origin: .zero, size: ring.frame.size)
+        current.strokeColor = .white
+        current.fillColor = .clear
+        current.lineWidth = 10
+        current.lineCap = .round
+        current.path = {
+            $0.addArc(center: .init(x: 120, y: 120), radius: 115, startAngle: .pi / 2, endAngle: .pi / 2 + (.pi * -2 * .init(progress.percentage)), clockwise: true)
+            return $0
+        } (CGMutablePath())
+        
+        current.add({
+            $0.duration = 2
+            $0.fromValue = 0
+            $0.toValue = 1
+            $0.timingFunction = .init(name: .easeInEaseOut)
+            return $0
+        } (CABasicAnimation(keyPath: "strokeEnd")), forKey: "strokeEnd")
+        
+        let gradient = Gradient()
+        gradient.frame = ring.frame
+        gradient.startPoint = .init(x: 0.5, y: 0)
+        gradient.endPoint = .init(x: 0.5, y: 1)
+        gradient.locations = [0, 1]
+        gradient.colors = [NSColor.systemBlue.cgColor, NSColor.quaternaryLabelColor.cgColor]
+        gradient.mask = current
+        contentViewController!.view.layer!.addSublayer(gradient)
+        
         let columns = counter(title: "Columns", value: cloud.archive.value[board].count)
         let cards = counter(title: "Cards", value: progress.cards)
         let completed = counter(title: "Done", value: progress.done)
         
         let percent = Text(vibrancy: true)
-        percent.stringValue = NumberFormatter.percent.string(from: .init(value: progress.percentage)) ?? ""
         percent.font = .monoDigit(style: .largeTitle, weight: .light)
         percent.textColor = .labelColor
         view.addSubview(percent)
         
-        var left = view.leftAnchor
-        [columns, cards, completed]
+        var right = view.rightAnchor
+        [completed, cards, columns]
             .forEach {
                 view.addSubview($0)
-                $0.leftAnchor.constraint(equalTo: left, constant: 30).isActive = true
-                left = $0.rightAnchor
+                $0.rightAnchor.constraint(equalTo: right, constant: -35).isActive = true
+                right = $0.leftAnchor
             }
         
-        columns.topAnchor.constraint(equalTo: view.topAnchor, constant: 30).isActive = true
+        columns.topAnchor.constraint(equalTo: view.topAnchor, constant: 40).isActive = true
         cards.topAnchor.constraint(equalTo: columns.topAnchor).isActive = true
         completed.topAnchor.constraint(equalTo: columns.topAnchor).isActive = true
-        percent.centerYAnchor.constraint(equalTo: completed.centerYAnchor).isActive = true
-        percent.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -30).isActive = true
+        percent.centerYAnchor.constraint(equalTo: view.bottomAnchor, constant: -160).isActive = true
+        percent.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        let timer = Timer.publish(every: 0.02, on: .main, in: .common).autoconnect()
+        var counter = Double()
+        var sub: AnyCancellable?
+        sub = timer.sink { _ in
+            if counter < progress.percentage {
+                counter += progress.percentage / 75
+            } else {
+                timer.upstream.connect().cancel()
+                sub?.cancel()
+                counter = progress.percentage
+            }
+            percent.stringValue = NumberFormatter.percent.string(from: .init(value: counter)) ?? ""
+        }
     }
     
     private func counter(title: String, value: Int) -> Text {
