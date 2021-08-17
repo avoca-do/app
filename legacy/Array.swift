e3b63a75f74439b35dc10cca337071ee790fc7ab
vaux <zero.griffin@gmail.com>
2021-08-17 16:48:50 +0300

    Remove legacy

--- legacy/Array.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-extension Array {
-    func transform(transform: (Self) -> Void) {
-        transform(self)
-    }
-}
--- legacy/NSAttributedString.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-extension NSAttributedString {
-    class func make(_ strings: [NSAttributedString]) -> NSAttributedString {
-        let mutable = NSMutableAttributedString()
-        strings.forEach(mutable.append)
-        return mutable
-    }
-}
