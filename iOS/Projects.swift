95cdb6e2fe4d39671478099a8e0c1b544f770d7e
vaux <zero.griffin@gmail.com>
2021-08-18 15:36:28 +0300

    Update projects

---
+++ iOS/Projects.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+struct Projects: View {
+    @Binding var session: Session
+    
+    var body: some View {
+        ZStack {
+            Color(.secondarySystemBackground)
+                .edgesIgnoringSafeArea(.all)
+            ScrollView {
+                
+            }
+        }
+    }
+}
--- iOS/Window.swift
+++ iOS/Window.swift
@@ -2,12 +2,11 @@ import SwiftUI
 
 struct Window: View {
     @Binding var session: Session
-    @Namespace private var namespace
     
     var body: some View {
         switch session.section {
         case .projects:
-            Circle()
+            Projects(session: $session)
         }
     }
 }
