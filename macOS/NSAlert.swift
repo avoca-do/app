79728d6b491776b2d1093014820246a396a52120
vaux <zero.griffin@gmail.com>
2021-08-22 12:31:14 +0200

    Make delete card easier

--- macOS/NSAlert.swift
+++ macOS/NSAlert.swift
@@ -3,43 +3,50 @@ import Kanban
 
 extension NSAlert {
     class func delete(path: Path) {
+        switch path {
+        case .board:
+            confirm(path: path, message: cloud.archive.value[path.board].name)
+        case .column:
+            confirm(path: path, message: cloud.archive.value[path.board][path.column].name)
+        default:
+            confirmed(path: path)
+        }
+    }
+    
+    private class func confirm(path: Path, message: String) {
         let alert = NSAlert()
         alert.alertStyle = .warning
         alert.icon = .init(systemSymbolName: "trash", accessibilityDescription: nil)
         alert.messageText = ""
+        alert.informativeText = message
         
-        switch path {
-        case .board:
-            alert.informativeText = cloud.archive.value[path.board].name
-        case .column:
-            alert.informativeText = cloud.archive.value[path.board][path.column].name
-        case .card:
-            alert.informativeText = cloud.archive.value[path.board][path.column][path.card].content
-        }
-        
-        let delete = alert.addButton(withTitle: NSLocalizedString("DELETE", comment: ""))
-        let cancel = alert.addButton(withTitle: NSLocalizedString("CANCEL", comment: ""))
+        let delete = alert.addButton(withTitle: "DELETE")
+        let cancel = alert.addButton(withTitle: "CANCEL")
         delete.keyEquivalent = "\r"
         cancel.keyEquivalent = "\u{1b}"
         if alert.runModal().rawValue == delete.tag {
-            switch path {
-            case .board:
-                Notifications.send(message: "Deleted project")
-                
-                session.select.send(nil)
-                session.state.send(.none)
-                cloud.delete(board: path.board)
-            case .column:
-                Notifications.send(message: "Deleted column")
-                
-                session.cancel()
-                cloud.delete(board: path.board, column: path.column)
-            case .card:
-                Notifications.send(message: "Deleted card")
-                
-                session.cancel()
-                cloud.delete(board: path.board, column: path.column, card: path.card)
-            }
+            confirmed(path: path)
+        }
+    }
+    
+    private class func confirmed(path: Path) {
+        switch path {
+        case .board:
+            Notifications.send(message: "Deleted project")
+            
+            session.select.send(nil)
+            session.state.send(.none)
+            cloud.delete(board: path.board)
+        case .column:
+            Notifications.send(message: "Deleted column")
+            
+            session.cancel()
+            cloud.delete(board: path.board, column: path.column)
+        case .card:
+            Notifications.send(message: "Deleted card")
+            
+            session.cancel()
+            cloud.delete(board: path.board, column: path.column, card: path.card)
         }
     }
 }
